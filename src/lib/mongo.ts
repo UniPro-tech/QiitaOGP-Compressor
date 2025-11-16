d17d5eefec11f6f4d75d0d5915aa5ad9d6fee8cb
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-11-17 08:47:55 +0900

    開発環境を整える

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -6,3 +6,27 @@ if (!uri) {
   throw new Error("MONGO_URI environment variable is not defined");
 }
 export const MongoConnection = new MongoClient(uri);
+export const Database = MongoConnection.db("qiita");
+
+const initialization = async () => {
+  try {
+    await MongoConnection.connect();
+    console.log("Connected to MongoDB");
+
+    // Ensure the 'ogps' collection exists
+    await createOGPCollection();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+const createOGPCollection = async () => {
+  const collections = await Database.listCollections().toArray();
+  const collectionNames = collections.map((col) => col.name);
+  if (!collectionNames.includes("ogps")) {
+    await Database.createCollection("ogps");
+    console.log("Created 'ogps' collection in the database.");
+  }
+};
+
+initialization();
