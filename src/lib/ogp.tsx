b180ac5b30d87bc5fe5ef9fbd67204ab80afff5f
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-11-28 15:46:18 +0900

    urlを正しく取得する

--- src/lib/ogp.tsx
+++ src/lib/ogp.tsx
@@ -1,5 +1,6 @@
 "use server";
 import { Metadata } from "next";
+import { headers } from "next/headers";
 
 export const getMetadata = async (itemid: string, userid?: string) => {
   const metadataRes = await fetch(
@@ -8,13 +9,15 @@ export const getMetadata = async (itemid: string, userid?: string) => {
       : `http://localhost:3000/api/items/${itemid}`
   );
   const data = (await metadataRes.json()).data;
+  const headerList = await headers();
+  const baseUrl = headerList.get("x-base-url");
   const metadata: Metadata = {
     title: data.title,
     description: data.description,
     openGraph: {
       title: data.title,
       description: data.description,
-      images: `http://localhost:3000/api/${
+      images: `${baseUrl}/api/${
         userid ? userid + "/" : ""
       }items/${itemid}/photo`,
       siteName: data.siteName,
--- src/proxy.ts
+++ src/proxy.ts
@@ -4,6 +4,12 @@ import { NextRequest } from "next/server";
 export async function proxy(req: NextRequest) {
   const requestHeaders = new Headers(req.headers);
   requestHeaders.set("x-url", req.url);
+  const originaHost = req.headers.get("host") || "localhost:3000";
+  requestHeaders.set("x-origin-host", originaHost);
+  const protocol = req.headers.get("x-forwarded-proto") || "http";
+  requestHeaders.set("x-origin-proto", protocol);
+  const baseUrl = `${protocol}://${originaHost}`;
+  requestHeaders.set("x-base-url", baseUrl);
 
   return NextResponse.next({
     request: {
