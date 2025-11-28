603c5840ce877a9da103757a26f0f2e97bca2a55
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-11-28 15:10:39 +0900

    miss

--- src/app/api/[uid]/items/[slug]/photo/route.ts
+++ src/app/api/[uid]/items/[slug]/photo/route.ts
@@ -1,12 +1,12 @@
 "use server";
+import { NextRequest } from "next/server";
 import { getImage } from "@/lib/api/image";
 
-export const GET = async ({
-  params,
-}: {
-  params: Promise<{ slug: string; uid: string }>;
-}) => {
-  const { slug, uid } = await params;
+export const GET = async (
+  _req: NextRequest,
+  { params }: { params: { uid: string; slug: string } }
+) => {
+  const { slug, uid } = params;
   const imageRes = await getImage(slug, uid);
   return new Response(imageRes.body, {
     headers: {
--- src/app/api/items/[slug]/photo/route.ts
+++ src/app/api/items/[slug]/photo/route.ts
@@ -1,11 +1,11 @@
 "use server";
 import { getImage } from "@/lib/api/image";
+import { NextRequest } from "next/server";
 
-export const GET = async ({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) => {
+export const GET = async (
+  _req: NextRequest,
+  { params }: { params: Promise<{ slug: string }> }
+) => {
   const { slug } = await params;
   const imageRes = await getImage(slug);
   return new Response(imageRes.body, {
